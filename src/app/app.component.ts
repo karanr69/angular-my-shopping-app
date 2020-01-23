dea9ecc2b7cfea4b6f4b17b5e2190aca6c88f922
Karan Singh <ksingh02@in.ibm.com>
2020-01-23 20:16:48 +0530

    footer image and chatbot integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,9 @@ import { ToastrService } from 'ngx-toastr';
 export class AppComponent {
   title = 'kool-shopping';
   constructor(private toastr: ToastrService) { }
+
+  openForm(){
+    window.open('https://assistant-chat-us-east.watsonplatform.net/web/public/8740773e-7ff7-44df-b840-9a502e820678');
+
+  }
 }
