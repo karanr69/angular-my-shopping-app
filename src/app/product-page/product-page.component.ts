a5a85784b1e720c78cbba96b6d707725e32a2d4a
Karan Singh <ksingh02@in.ibm.com>
2020-02-17 15:46:25 +0530

    login

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -13,6 +13,7 @@ export class ProductPageComponent implements OnInit {
    
    }
 private sub:any;
+public products:any;
 public similarproduct:any;
 public product1=[{"ITEM_NUMBER":"1001","DESCRIPTION":"Reflex Womenâ€™s Track Jacket","LONG_DESCRIPTION":"90 Degree By Reflex Womenâ€™s Lightweight, Full Zip Running Track Jacket","CATALOGUE_CATEGORY":"53102901","SKU_UNIT_OF_MEASURE":"Each","STYLE_ITEM":"1000","SKU_ATTRIBUTE1":"Size","SKU_ATTRIBUTE2":"Color","SKU_ATTRIBUTE3":null,"SKU_ATTRIBUTE4":null,"SKU_ATTRIBUTE5":null,"SKU_ATTRIBUTE6":null,"SKU_ATTRIBUTE_VALUE1":"Medium","SKU_ATTRIBUTE_VALUE2":"Black","SKU_ATTRIBUTE_VALUE3":null,"SKU_ATTRIBUTE_VALUE4":null,"SKU_ATTRIBUTE_VALUE5":null,"SKU_ATTRIBUTE_VALUE6":null}];
 public product2=[{"PRICE_ID":"1","ITEM_NUMBER":"1001","LIST_PRICE":"39.33","DISCOUNT":"0.05","IN_STOCK":"Yes","PRICE_EFFECTIVE_DATE":"2029-09-19T00:00:00.000Z"}];
@@ -43,7 +44,7 @@ private page:any;
       let description=this.product1[0].LONG_DESCRIPTION;
       if(description!=null){
         this.restApi.getAllSearchProductList(description).subscribe((data:any)=>{
-         
+         this.products=data.response;
 
         });
       }
